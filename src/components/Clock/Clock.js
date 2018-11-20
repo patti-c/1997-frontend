b0b649b22326c054c230689945ff014b96dec724
patti-c <patricia.carter@flatironschool.com>
2018-11-20 11:01:39 -0500

    Very basic styling on homepage + navbar

---
+++ src/components/Clock/Clock.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Clock extends Component {
+  render() {
+    return (
+      <div className="clock">
+        <p>9:30AM</p>
+      </div>
+    )
+  }
+}
+
+export default Clock
---
+++ src/containers/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import './Navbar.css'
+import Clock from '../../components/Clock/Clock.js'
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div className="navbar">
+        <Clock />
+      </div>
+
+    )
+  }
+}
+
+export default Navbar

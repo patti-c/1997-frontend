df2c1a1668e7d08d49a53ea217cfd06d325ebf44
patti-c <patricia.carter@flatironschool.com>
2018-12-06 11:46:31 -0500

    changed api ws root

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,5 @@
 export const API_ROOT = 'https://api-1997.herokuapp.com/api/v1/';
-export const API_WS_ROOT = 'ws://api-1997.herokuapp.com/cable';
+export const API_WS_ROOT = 'wss://api-1997.herokuapp.com/cable';
 export const HEADERS = {
   'Content-Type': 'application/json',
   Accept: 'application/json',

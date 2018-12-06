bc3790b4c4c0cee3eb4c55c4dfe1ed1ab2971546
patti-c <patricia.carter@flatironschool.com>
2018-12-06 11:02:49 -0500

    changing api root

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,5 @@
-export const API_ROOT = 'http://localhost:3000/api/v1/';
-export const API_WS_ROOT = 'ws://localhost:3000/cable';
+export const API_ROOT = 'https://api-1997.herokuapp.com/api/v1/';
+export const API_WS_ROOT = 'ws://api-1997.herokuapp.com/cable';
 export const HEADERS = {
   'Content-Type': 'application/json',
   Accept: 'application/json',
